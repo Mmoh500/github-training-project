b575e22bd5500104fc632f6f70f79800f354ef1f
mohammed al mansi <msimoh2004@gmail.com>
2026-05-25 11:50:12 +0300

    Added checkout functionality

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
   Item pizza = Item(id: 1, name: "Pizza", price: 35.9, qty: 1);
   Item burger = Item(id: 2, name: "Burger", price: 25.5, qty: 1);
   Item juice = Item(id: 3, name: "Juice", price: 25.5, qty: 1);
+  Item pizzaHot = Item(id: 4, name: "pizzaHot", price: 50.0, qty: 1);
+ 
 
   Checkout cart = Checkout();
 
